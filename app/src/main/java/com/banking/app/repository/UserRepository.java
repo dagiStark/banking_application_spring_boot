82e8d5df077c570690e9314da42ae050226b31ee
dagiStark <160214504+dagiStark@users.noreply.github.com>
2025-02-03 15:52:53 +0300

    user database operations

---
+++ app/src/main/java/com/banking/app/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.banking.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.banking.app.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Boolean existsByEmail(String email);
+}
